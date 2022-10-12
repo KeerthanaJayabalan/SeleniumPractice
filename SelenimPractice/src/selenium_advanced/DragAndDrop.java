f371f0cbe9c29769ff65615c15862c2e5382b0ad
indian <indian@192.168.0.105>
2022-10-12 23:24:42 +0530

    Drag and drop

--- SelenimPractice/src/selenium_advanced/DragAndDrop.java
+++ SelenimPractice/src/selenium_advanced/DragAndDrop.java
@@ -11,20 +11,20 @@ import org.openqa.selenium.interactions.Actions;
 public class DragAndDrop {
 
 	public static void main(String[] args) {
-		
+
 		System.setProperty("webdriver.chrome.driver", "F:\\Testing\\Selenium\\chromedriver.exe");
 		WebDriver driver=new ChromeDriver();
 		driver.get("https://jqueryui.com/droppable/");
 
-	
-driver.switchTo().frame(0);
 
-WebElement From=driver.findElement(By.id("draggable"));
-WebElement To=driver.findElement(By.id("droppable"));
-		
-Actions actions=new Actions(driver);
-//actions.clickAndHold(From).moveToElement(To).release(To).build().perform();
-actions.dragAndDrop(From, To).build().perform();
+		driver.switchTo().frame(0);
+
+		WebElement From=driver.findElement(By.id("draggable"));
+		WebElement To=driver.findElement(By.id("droppable"));
+
+		Actions actions=new Actions(driver);
+		//actions.clickAndHold(From).moveToElement(To).release(To).build().perform();
+		actions.dragAndDrop(From, To).build().perform();
 
 	}
 
