00c5d12c1b2a7b2dc31b41d9eac92ff4431e397e
indian <indian@192.168.0.105>
2022-10-17 16:43:51 +0530

    Image

---
+++ SelenimPractice/src/selenium_advanced/Image.java
@@ -0,0 +1,37 @@
+package selenium_advanced;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.devtools.v85.debugger.model.CallFrameId;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class Image {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "F:\\Testing\\Selenium\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();  // Chrome browser 
+		driver.navigate().to("http://testleaf.herokuapp.com/pages/Image.html");
+		
+		
+		
+		WebElement firstImg=driver.findElement(By.xpath("//*[@id=\'contentblock\']/section/div[1]/div/div/img"));
+		firstImg.click();
+		
+		driver.navigate().back();
+		
+		WebElement secImg=driver.findElement(By.xpath("//*[@id=\'contentblock\']/section/div[2]/div/div/img"));
+		//"naturalWidth=0"
+		
+		if (secImg.getAttribute("naturalWidth").equals("0")) {
+			System.out.println("Image is broken");
+		}else {
+			System.out.println("Image is not broken");
+		}
+		
+	}
+
+}
